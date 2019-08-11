d1b3e6f08cd1da0c19556cbda1f71ef957c72d41
Yul Per <yuliiaperedrii.qa@gmail.com>
2019-08-11 00:20:56 -0700

    initial commit

---
+++ src/main/java/core/App.java
@@ -0,0 +1,5 @@
+package core;
+
+//public static void main(String[] args) {
+//    System.out.println("You have to use -cp option [java -cp ./target/HW_44-1.0-jar-with-dependencies.jar core.Firefox]");
+//}
